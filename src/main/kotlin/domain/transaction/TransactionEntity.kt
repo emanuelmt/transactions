e8f788f9bbbca105189caa6f27453280d5703aff
Emanuel Marques <emanuelmtmarques@gmail.com>
2025-02-11 20:53:59 -0300

    feat: define merchant name to balance type logic

--- src/main/kotlin/domain/transaction/TransactionEntity.kt
+++ src/main/kotlin/domain/transaction/TransactionEntity.kt
@@ -31,3 +31,30 @@ fun merchantTypeToBalanceType(merchantType: String): BalanceType {
         else -> BalanceType.CASH
     }
 }
+
+fun merchantNameToBalanceType(merchantName: String, merchantType: String): BalanceType {
+    val bestMatch = MerchantClassifierRules.mapNotNull { rule ->
+        rule.regex.find(merchantName)?.let {
+            val score = rule.weight
+            rule to score
+        }
+    }.maxByOrNull { it.second }
+
+    return bestMatch?.first?.category ?: merchantTypeToBalanceType(merchantType)
+}
+
+data class MerchantRule(val regex: Regex, val category: BalanceType, val weight: Int)
+
+private val MerchantClassifierRules = listOf(
+    MerchantRule("(?i).*uber eats.*".toRegex(), BalanceType.MEAL, 10),
+    MerchantRule("(?i).*uber trip.*".toRegex(), BalanceType.CASH, 10),
+    MerchantRule("(?i).*ifood.*".toRegex(), BalanceType.MEAL, 10),
+    MerchantRule("(?i).*uber.*".toRegex(), BalanceType.CASH, 9),
+    MerchantRule("(?i).*padaria.*".toRegex(), BalanceType.FOOD, 8),
+    MerchantRule("(?i).*mercado.*".toRegex(), BalanceType.FOOD, 8),
+    MerchantRule("(?i).*restaurante.*".toRegex(), BalanceType.MEAL, 8),
+    MerchantRule("(?i).*churrascaria.*".toRegex(), BalanceType.MEAL, 8),
+    MerchantRule("(?i).*sabor.*".toRegex(), BalanceType.MEAL, 6),
+    MerchantRule("(?i).*picpay.*".toRegex(), BalanceType.CASH, 5),
+    MerchantRule("(?i).*pag.*".toRegex(), BalanceType.MEAL, 5),
+)
