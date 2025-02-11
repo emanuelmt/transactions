bdd4230aebf11a3c37a2edb8e0d34d8465739011
Emanuel Marques <emanuelmtmarques@gmail.com>
2025-02-10 21:35:43 -0300

    refactor: adjust serialization exception handler

--- src/main/kotlin/ExceptionHandler.kt
+++ src/main/kotlin/ExceptionHandler.kt
@@ -1,15 +1,21 @@
 package dev.emanuelmt
 
 import io.ktor.http.*
+import io.ktor.serialization.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
 
 @Serializable
 data class ValidationErrorResponse(val message: String, val reasons: List<String>)
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureExceptionHandler() {
     install(StatusPages) {
         exception<RequestValidationException> { call, cause ->
@@ -17,6 +23,19 @@ fun Application.configureExceptionHandler() {
                 HttpStatusCode.BadRequest,
                 ValidationErrorResponse("some input params are invalid", cause.reasons),
             )
+        }
+        exception<BadRequestException> { call, cause ->
+            when (cause.cause) {
+                is MissingFieldException -> {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("error" to "required input param ${cause.message} not passed"))
+                }
+                is JsonConvertException, is SerializationException -> {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("error" to "invalid input: ${cause.message}"))
+                }
+                else -> {
+                    call.respond(HttpStatusCode.BadRequest, mapOf("error" to "invalid request: ${cause.message}"))
+                }
+            }
         }
    }
 }
--- src/main/kotlin/Serialization.kt
+++ src/main/kotlin/Serialization.kt
@@ -3,9 +3,16 @@ package dev.emanuelmt
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = false
+            isLenient = false
+            encodeDefaults = true
+
+        })
     }
 }
