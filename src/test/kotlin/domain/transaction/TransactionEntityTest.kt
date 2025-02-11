25485733343d579f5beae5577d1b1d191e033377
Emanuel Marques <emanuelmtmarques@gmail.com>
2025-02-11 20:56:01 -0300

    test: merchant to balance type function

--- src/test/kotlin/domain/transaction/TransactionEntityTest.kt
+++ src/test/kotlin/domain/transaction/TransactionEntityTest.kt
@@ -21,6 +21,24 @@ class TransactionEntityTest {
                 Arguments.arguments(defaultTransaction.copy(merchantType = "5812"), BalanceType.MEAL)
             )
         }
+
+        @JvmStatic
+        fun getMerchantNameExamples(): Stream<Arguments> {
+            val default = TransactionEntity("123", "123", "123", "123", 15000)
+            return Stream.of(
+                Arguments.arguments(default.copy(merchantName = "UBER EATS São Paulo", merchantType = "0000"), BalanceType.MEAL),
+                Arguments.arguments(default.copy(merchantName = "UBER TRIP Campinas", merchantType = "9999"), BalanceType.CASH),
+                Arguments.arguments(default.copy(merchantName = "UBER Campinas", merchantType = "9999"), BalanceType.CASH),
+                Arguments.arguments(default.copy(merchantName = "Padaria do Zé", merchantType = "5411"), BalanceType.FOOD),
+                Arguments.arguments(default.copy(merchantName = "Ifood", merchantType = "5411"), BalanceType.MEAL),
+                Arguments.arguments(default.copy(merchantName = "PICPAY*PADARIABOMSABOR", merchantType = "5411"), BalanceType.FOOD),
+                Arguments.arguments(default.copy(merchantName = "PICPAY*SaborDaRoca", merchantType = "5411"), BalanceType.MEAL),
+                Arguments.arguments(default.copy(merchantName = "PICPAY*Sabor", merchantType = "5411"), BalanceType.MEAL),
+                Arguments.arguments(default.copy(merchantName = "Restaurante Central", merchantType = "5811"), BalanceType.MEAL),
+                Arguments.arguments(default.copy(merchantName = "Mercado Novo", merchantType = "9999"), BalanceType.FOOD),
+                Arguments.arguments(default.copy(merchantName = "Teste de fallback", merchantType = "5812"), BalanceType.MEAL),
+            )
+        }
     }
 
     @ParameterizedTest
@@ -29,4 +47,12 @@ class TransactionEntityTest {
         val result = getTransactionBalanceType(transaction)
         assertEquals(expectedBalanceType, result, "Test failed for transaction: $transaction")
     }
+
+    @ParameterizedTest
+    @MethodSource("getMerchantNameExamples")
+    fun testGetTransactionBalanceTypeByMerchantParam(transaction: TransactionEntity, expectedBalanceType: BalanceType) {
+        val balanceType = merchantNameToBalanceType(transaction.merchantName, transaction.merchantType)
+        assertEquals(expectedBalanceType, balanceType, "Failed with merchant: ${transaction.merchantName}")
+    }
+
 }
